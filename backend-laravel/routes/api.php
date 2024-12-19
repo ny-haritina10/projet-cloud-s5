1502f5d36b2de504bb589a8e678d1bfc2bf6b601
ny-haritina10 <142341722+ny-haritina10@users.noreply.github.com>
2024-12-19 10:21:46 +0100

    Update api.php

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SignupController;
-use App\Http\Controllers\Api\SignupController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\TokenController;
